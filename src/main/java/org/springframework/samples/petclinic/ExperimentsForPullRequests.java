4db3244ce1072849be74aeee351d3f10557edeca
anton <ant.arhipov@gmail.com>
2023-09-05 12:21:05 +0300

    Added more code examples for static analysis

--- src/main/java/org/springframework/samples/petclinic/ExperimentsForPullRequests.java
+++ src/main/java/org/springframework/samples/petclinic/ExperimentsForPullRequests.java
@@ -2,10 +2,29 @@ package org.springframework.samples.petclinic;
 
 import org.springframework.lang.Nullable;
 
+import java.util.Optional;
+
 public class ExperimentsForPullRequests {
 
 	public static void foo(@Nullable String s) {
 		System.out.println("String length: " + s.length());
+	}
+
+	public static void bar() {
+		while (true) {
+			System.out.println("test");
+		}
+	}
+
+	public static void baz(Optional<String> os) {
+		System.out.println(os.get());
+		boo();
+	}
+
+	@Deprecated(forRemoval = true)
+	public static String boo(){
+		System.out.println();
+		return "";
 	}
 
 }
