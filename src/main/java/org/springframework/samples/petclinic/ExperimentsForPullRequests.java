2ecec297c84e2eb9712efffaf3c1998dc656a982
anton <ant.arhipov@gmail.com>
2023-09-07 13:08:58 +0300

    QD-001: fixing issues

--- src/main/java/org/springframework/samples/petclinic/ExperimentsForPullRequests.java
+++ src/main/java/org/springframework/samples/petclinic/ExperimentsForPullRequests.java
@@ -198,18 +198,6 @@ public class ExperimentsForPullRequests {
 		System.out.println("String length: " + s.length());
 	}
 
-	public static void foo56(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo57(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo58(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
 	public static void bar() {
 		while (true) {
 			System.out.println("test");
