ddf4143fd3364f87657e50aebf59e9a4c7c95d3b
anton <ant.arhipov@gmail.com>
2023-09-07 13:57:18 +0300

    QD-001: fixing issues

--- src/main/java/org/springframework/samples/petclinic/ExperimentsForPullRequests.java
+++ src/main/java/org/springframework/samples/petclinic/ExperimentsForPullRequests.java
@@ -14,190 +14,6 @@ public class ExperimentsForPullRequests {
 		System.out.println("String length: " + s.length());
 	}
 
-	public static void foo2(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo3(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo4(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo5(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo6(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo7(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo8(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo9(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo10(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo11(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo12(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo13(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo14(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo15(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo16(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo17(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo18(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo19(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo20(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo21(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo22(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo23(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo24(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo25(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo26(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo27(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo28(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo29(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo30(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo31(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo32(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo33(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo34(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo35(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo36(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo37(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo38(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo41(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo42(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo43(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo44(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo45(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo46(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo47(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo48(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
-	public static void foo55(@Nullable String s) {
-		System.out.println("String length: " + s.length());
-	}
-
 	public static void bar() {
 		while (true) {
 			System.out.println("test");
@@ -206,7 +22,7 @@ public class ExperimentsForPullRequests {
 
 	public static void baz(Optional<String> os) {
 		System.out.println(os.get());
-		boo();
+//		boo();
 	}
 
 	@Deprecated(forRemoval = true)
@@ -226,10 +42,8 @@ public class ExperimentsForPullRequests {
 		}
 
 		int x = 18;
-		x *= 3 / 2; // doesn't change x because of the integer division result
+//		x *= 3 / 2; // doesn't change x because of the integer division result
 
-		while (true) {
-		}
 	}
 
 }
